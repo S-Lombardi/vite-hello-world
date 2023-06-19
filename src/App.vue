b002cb4baff064dea2a3847f4f6f458a3783d3a0
S-Lombardi <silo.lombardi@gmail.com>
2023-06-19 16:53:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue'
+</script>
+
+<template>
+
+  <AppTitle msg="La mia prima app con Vite" />
+</template>
+
+<style scoped>
+
+</style>
